233e21dc7b42c1df93a268ebe9c9176656bf7f53
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-05-13 20:57:51 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,30 @@ import { useFetch } from './components/Hook/useFetch'
 
 
 function App({}) {
-  const[ value,setVale] =useState()
-  const {data} = useFetch(`/products/category/${value}`,value)
+
+
+  const[ value,setVale] =useState('All')
+  let javob = ""
+  if(value == "All"){
+    javob = "/products"
+  }
+  else{
+    javob = `/products/category/${value}`
+  }
+  console.log(value);
+  const {data} = useFetch(javob,value)
   const {data:categories} =useFetch('/products/categories')
   let btns= categories?.data?.map((el,inx) =>(
     <button onClick={() =>setVale(el)}  key={inx}>{el}</button>
   ))
+
+  
   return (
     <>
 
     <div className="hammasi " >
   <div className="all container">
-  <button onClick={() =>setVale(data?.data?.data?.products)} >
+  <button onClick={() =>setVale('All')} >
     All
   </button>
   {btns}
